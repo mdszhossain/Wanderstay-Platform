911367cbe78e74aed9555efc3b67a2e0b35da5b2
= <=>
2026-07-01 19:02:39 +0600

    REST API created. routing working perfectly

--- app.js
+++ app.js
@@ -2,7 +2,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const methodOverride = require("method-override");
+const path = require("path");
 const app = express();
+
+// middlewares and setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // database connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderstay";
@@ -22,17 +31,50 @@ app.get("/", (req, res) => {
   res.send("Hi! I am root");
 });
 
-app.get("/testListing", async(req, res) => {
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Potenga, Chittagong",
-        country: "Bangladesh",
-    });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
+// Index Route
+app.get("/listings", async(req, res) => {
+    let allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+// New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+app.post("/listings", async(req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+// Edit Route
+app.get("/listings/:id/edit", async(req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+// Update Route
+app.patch("/listings/:id", async(req, res) => {
+    let {id} = req.params;
+    const newListing = req.body.listing;
+    const result = await Listing.updateOne({_id: id}, newListing, {new: true});
+    res.redirect(`/listings`);
+});
+
+// Delete Route
+app.delete("/listings/:id", async(req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    res.redirect('/listings');
+});
+
+// Show Route
+app.get("/listings/:id", async(req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
 });
 
 // server listening
