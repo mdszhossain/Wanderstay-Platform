2d2fe0777f2d1b34c9ee157a56280eac855906f4
mdszhossain <personalmailshazzad@gmail.com>
2026-07-11 00:47:37 +0600

    Update: A little update on code

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public/css")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
@@ -34,49 +34,51 @@ app.get("/", (req, res) => {
 });
 
 // Index Route
-app.get("/listings", async(req, res) => {
-    let allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
 // New Route
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
+  res.render("listings/new.ejs"); 
 });
 
-app.post("/listings", async(req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 // Edit Route
-app.get("/listings/:id/edit", async(req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
 // Update Route
-app.patch("/listings/:id", async(req, res) => {
-    let {id} = req.params;
-    const newListing = req.body.listing;
-    const result = await Listing.updateOne({_id: id}, newListing, {new: true});
-    res.redirect(`/listings`);
+app.patch("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const newListing = req.body.listing;
+  const result = await Listing.updateOne({ _id: id }, newListing, {
+    new: true,
+  });
+  res.redirect(`/listings`);
 });
 
 // Delete Route
-app.delete("/listings/:id", async(req, res) => {
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    res.redirect('/listings');
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 });
 
 // Show Route
-app.get("/listings/:id", async(req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 // server listening
